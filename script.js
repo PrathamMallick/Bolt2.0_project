37f46006a3b616ff5ce2930d0cdbb420bea9cc88
ArinSingla09 <arin.singla.09@gmail.com>
2024-02-23 01:31:14 +0530

    Map Page

---
+++ script.js
@@ -0,0 +1,385 @@
+function ANhover() {
+    document.querySelector(".AN-info").style.display="flex";
+    document.querySelector(".AN-info").style.flexDirection="column";
+    document.querySelector(".AN-info").style.alignItems="center";
+    document.querySelector(".AN-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function ANhide(){
+    document.querySelector(".AN-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function TLhover() {
+    document.querySelector(".TL-info").style.display="flex";
+    document.querySelector(".TL-info").style.flexDirection="column";
+    document.querySelector(".TL-info").style.alignItems="center";
+    document.querySelector(".TL-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function TLhide(){
+    document.querySelector(".TL-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function ADhover() {
+    document.querySelector(".AD-info").style.display="flex";
+    document.querySelector(".AD-info").style.flexDirection="column";
+    document.querySelector(".AD-info").style.alignItems="center";
+    document.querySelector(".AD-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function ADhide(){
+    document.querySelector(".AD-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function ARhover() {
+    document.querySelector(".AR-info").style.display="flex";
+    document.querySelector(".AR-info").style.flexDirection="column";
+    document.querySelector(".AR-info").style.alignItems="center";
+    document.querySelector(".AR-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function ARhide(){
+    document.querySelector(".AR-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function AShover() {
+    document.querySelector(".AS-info").style.display="flex";
+    document.querySelector(".AS-info").style.flexDirection="column";
+    document.querySelector(".AS-info").style.alignItems="center";
+    document.querySelector(".AS-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function AShide(){
+    document.querySelector(".AS-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function BIhover() {
+    document.querySelector(".BI-info").style.display="flex";
+    document.querySelector(".BI-info").style.flexDirection="column";
+    document.querySelector(".BI-info").style.alignItems="center";
+    document.querySelector(".BI-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function BIhide(){
+    document.querySelector(".BI-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function CHhover() {
+    document.querySelector(".CH-info").style.display="flex";
+    document.querySelector(".CH-info").style.flexDirection="column";
+    document.querySelector(".CH-info").style.alignItems="center";
+    document.querySelector(".CH-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function CHhide(){
+    document.querySelector(".CH-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function CThover() {
+    document.querySelector(".CT-info").style.display="flex";
+    document.querySelector(".CT-info").style.flexDirection="column";
+    document.querySelector(".CT-info").style.alignItems="center";
+    document.querySelector(".CT-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function CThide(){
+    document.querySelector(".CT-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function DLhover() {
+    document.querySelector(".DL-info").style.display="flex";
+    document.querySelector(".DL-info").style.flexDirection="column";
+    document.querySelector(".DL-info").style.alignItems="center";
+    document.querySelector(".DL-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function DLhide(){
+    document.querySelector(".DL-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function GOhover() {
+    document.querySelector(".GO-info").style.display="flex";
+    document.querySelector(".GO-info").style.flexDirection="column";
+    document.querySelector(".GO-info").style.alignItems="center";
+    document.querySelector(".GO-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function GOhide(){
+    document.querySelector(".GO-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function GJhover() {
+    document.querySelector(".GJ-info").style.display="flex";
+    document.querySelector(".GJ-info").style.flexDirection="column";
+    document.querySelector(".GJ-info").style.alignItems="center";
+    document.querySelector(".GJ-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function GJhide(){
+    document.querySelector(".GJ-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function HRhover() {
+    document.querySelector(".HR-info").style.display="flex";
+    document.querySelector(".HR-info").style.flexDirection="column";
+    document.querySelector(".HR-info").style.alignItems="center";
+    document.querySelector(".HR-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function HRhide(){
+    document.querySelector(".HR-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function HIhover() {
+    document.querySelector(".HI-info").style.display="flex";
+    document.querySelector(".HI-info").style.flexDirection="column";
+    document.querySelector(".HI-info").style.alignItems="center";
+    document.querySelector(".HI-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function HIhide(){
+    document.querySelector(".HI-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function JHhover() {
+    document.querySelector(".JH-info").style.display="flex";
+    document.querySelector(".JH-info").style.flexDirection="column";
+    document.querySelector(".JH-info").style.alignItems="center";
+    document.querySelector(".JH-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function JHhide(){
+    document.querySelector(".JH-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function KNhover() {
+    document.querySelector(".KN-info").style.display="flex";
+    document.querySelector(".KN-info").style.flexDirection="column";
+    document.querySelector(".KN-info").style.alignItems="center";
+    document.querySelector(".KN-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function KNhide(){
+    document.querySelector(".KN-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function KRhover() {
+    document.querySelector(".KR-info").style.display="flex";
+    document.querySelector(".KR-info").style.flexDirection="column";
+    document.querySelector(".KR-info").style.alignItems="center";
+    document.querySelector(".KR-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function KRhide(){
+    document.querySelector(".KR-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function MPhover() {
+    document.querySelector(".MP-info").style.display="flex";
+    document.querySelector(".MP-info").style.flexDirection="column";
+    document.querySelector(".MP-info").style.alignItems="center";
+    document.querySelector(".MP-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function MPhide(){
+    document.querySelector(".MP-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function MHhover() {
+    document.querySelector(".MH-info").style.display="flex";
+    document.querySelector(".MH-info").style.flexDirection="column";
+    document.querySelector(".MH-info").style.alignItems="center";
+    document.querySelector(".MH-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function MHhide(){
+    document.querySelector(".MH-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function MGhover() {
+    document.querySelector(".MG-info").style.display="flex";
+    document.querySelector(".MG-info").style.flexDirection="column";
+    document.querySelector(".MG-info").style.alignItems="center";
+    document.querySelector(".MG-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function MGhide(){
+    document.querySelector(".MG-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function MNhover() {
+    document.querySelector(".MN-info").style.display="flex";
+    document.querySelector(".MN-info").style.flexDirection="column";
+    document.querySelector(".MN-info").style.alignItems="center";
+    document.querySelector(".MN-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function MNhide(){
+    document.querySelector(".MN-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function MZhover() {
+    document.querySelector(".MZ-info").style.display="flex";
+    document.querySelector(".MZ-info").style.flexDirection="column";
+    document.querySelector(".MZ-info").style.alignItems="center";
+    document.querySelector(".MZ-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function MZhide(){
+    document.querySelector(".MZ-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function NGhover() {
+    document.querySelector(".NG-info").style.display="flex";
+    document.querySelector(".NG-info").style.flexDirection="column";
+    document.querySelector(".NG-info").style.alignItems="center";
+    document.querySelector(".NG-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function NGhide(){
+    document.querySelector(".NG-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function ORhover() {
+    document.querySelector(".OR-info").style.display="flex";
+    document.querySelector(".OR-info").style.flexDirection="column";
+    document.querySelector(".OR-info").style.alignItems="center";
+    document.querySelector(".OR-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function ORhide(){
+    document.querySelector(".OR-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function PDhover() {
+    document.querySelector(".PD-info").style.display="flex";
+    document.querySelector(".PD-info").style.flexDirection="column";
+    document.querySelector(".PD-info").style.alignItems="center";
+    document.querySelector(".PD-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function PDhide(){
+    document.querySelector(".PD-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function PJhover() {
+    document.querySelector(".PJ-info").style.display="flex";
+    document.querySelector(".PJ-info").style.flexDirection="column";
+    document.querySelector(".PJ-info").style.alignItems="center";
+    document.querySelector(".PJ-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function PJhide(){
+    document.querySelector(".PJ-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function RJhover() {
+    document.querySelector(".RJ-info").style.display="flex";
+    document.querySelector(".RJ-info").style.flexDirection="column";
+    document.querySelector(".RJ-info").style.alignItems="center";
+    document.querySelector(".RJ-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function RJhide(){
+    document.querySelector(".RJ-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function SKhover() {
+    document.querySelector(".SK-info").style.display="flex";
+    document.querySelector(".SK-info").style.flexDirection="column";
+    document.querySelector(".SK-info").style.alignItems="center";
+    document.querySelector(".SK-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function SKhide(){
+    document.querySelector(".SK-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function TNhover() {
+    document.querySelector(".TN-info").style.display="flex";
+    document.querySelector(".TN-info").style.flexDirection="column";
+    document.querySelector(".TN-info").style.alignItems="center";
+    document.querySelector(".TN-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function TNhide(){
+    document.querySelector(".TN-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function TPhover() {
+    document.querySelector(".TP-info").style.display="flex";
+    document.querySelector(".TP-info").style.flexDirection="column";
+    document.querySelector(".TP-info").style.alignItems="center";
+    document.querySelector(".TP-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function TPhide(){
+    document.querySelector(".TP-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function UPhover() {
+    document.querySelector(".UP-info").style.display="flex";
+    document.querySelector(".UP-info").style.flexDirection="column";
+    document.querySelector(".UP-info").style.alignItems="center";
+    document.querySelector(".UP-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function UPhide(){
+    document.querySelector(".UP-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function UKhover() {
+    document.querySelector(".UK-info").style.display="flex";
+    document.querySelector(".UK-info").style.flexDirection="column";
+    document.querySelector(".UK-info").style.alignItems="center";
+    document.querySelector(".UK-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function UKhide(){
+    document.querySelector(".UK-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function WBhover() {
+    document.querySelector(".WB-info").style.display="flex";
+    document.querySelector(".WB-info").style.flexDirection="column";
+    document.querySelector(".WB-info").style.alignItems="center";
+    document.querySelector(".WB-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function WBhide(){
+    document.querySelector(".WB-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function LKhover() {
+    document.querySelector(".LK-info").style.display="flex";
+    document.querySelector(".LK-info").style.flexDirection="column";
+    document.querySelector(".LK-info").style.alignItems="center";
+    document.querySelector(".LK-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function LKhide(){
+    document.querySelector(".LK-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function JKhover() {
+    document.querySelector(".JK-info").style.display="flex";
+    document.querySelector(".JK-info").style.flexDirection="column";
+    document.querySelector(".JK-info").style.alignItems="center";
+    document.querySelector(".JK-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function JKhide(){
+    document.querySelector(".JK-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
+function LAhover() {
+    document.querySelector(".LA-info").style.display="flex";
+    document.querySelector(".LA-info").style.flexDirection="column";
+    document.querySelector(".LA-info").style.alignItems="center";
+    document.querySelector(".LA-info").style.justifyContent="space-around";
+    document.querySelector(".default").style.display="none";
+}
+function LAhide(){
+    document.querySelector(".LA-info").style.display="none";
+    document.querySelector(".default").style.display="inline";
+}
